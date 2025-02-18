9d2248665df1ff9ad21a8f1e83250d7d57ff24eb
jayesh-patil123 <patiljayesh6908@gmail.com>
2025-02-18 22:27:40 +0530

    setup

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Ritesh',
-    author_email='riteshsingh9827@gmail.com',
+    author='Jayesh',
+    author_email='patiljayesh6908@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )

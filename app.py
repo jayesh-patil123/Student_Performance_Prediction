3187b6ad3e4a9f609ec57be15cb5aec98ae32b72
jayesh-patil123 <patiljayesh6908@gmail.com>
2025-02-26 22:29:18 +0530

    app and requirements

--- app.py
+++ app.py
@@ -40,5 +40,6 @@ def predict_datapoint():
         
         return render_template('home.html',results=results[0])
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=5000)

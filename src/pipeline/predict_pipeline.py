cbce66b72e03777705e1845594b98c36d3289046
jayesh-patil123 <patiljayesh6908@gmail.com>
2025-03-03 12:03:05 +0530

    pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -53,12 +53,12 @@ class CustomData:
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
-        self.lunch = lunch  # Fixed: Changed from int to str
+        self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
 
-    def get_data_as_dataframe(self):
+    def get_data_as_data_frame(self):
         """Converts the custom input data into a pandas DataFrame."""
         try:
             logging.info("Converting user input into DataFrame...")
